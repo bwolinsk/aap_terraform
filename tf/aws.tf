51c0847aa35c74511a96d6d2014ef71262c80fee
Cloud User <ec2-user@ip-10-0-20-83.ec2.internal>
2023-03-01 19:28:29 +0000

    Added eip association

--- tf/aws.tf
+++ tf/aws.tf
@@ -23,9 +23,18 @@ resource "aws_instance" "example" {
   ami = "ami-0c9978668f8d55984"
   instance_type = "t2.micro"
   key_name      = "controller_bart"
-  ecs_associate_public_ip_address = "true"
   count         = "1"
   subnet_id     = aws_subnet.tf_subnet.id
+}
+
+resource "aws_eip" "tf_eip" {
+  instance = aws_instance.example.id"
+  vpc = true
+}
+
+resource "aws_eip_association" "tf_eip_association" {
+  instance_id = aws_instance.example.id
+  allocation_id = "eipalloc-01da02aa45306e506"
 }
 
 output "address" {
