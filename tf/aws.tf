13283a860500969a2cb141456d9fe49147661f88
Cloud User <ec2-user@ip-10-0-20-83.ec2.internal>
2023-03-02 20:04:14 +0000

    Changed subnet to allow public ip

--- tf/aws.tf
+++ tf/aws.tf
@@ -21,6 +21,7 @@ resource "aws_internet_gateway" "tf_gateway" {
 resource "aws_subnet" "tf_subnet" {
   vpc_id     = aws_vpc.tf_vpc.id
   cidr_block = "192.168.22.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "tf_subnet"
