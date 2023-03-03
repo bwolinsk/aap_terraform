e209713eb318e821d423ac386ef932bc23d19311
Cloud User <ec2-user@ip-10-0-20-83.ec2.internal>
2023-03-03 17:00:40 +0000

    Route table association

--- tf/aws.tf
+++ tf/aws.tf
@@ -28,6 +28,11 @@ resource "aws_subnet" "tf_subnet" {
   }
 }
 
+resource "aws_route_table_association" "test" {
+  gateway_id     = aws_internet_gateway.tf_gateway.id
+  route_table_id = "rtb-0aeb94d3fa4e4fbc4"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0c9978668f8d55984"
   instance_type = "t2.micro"
