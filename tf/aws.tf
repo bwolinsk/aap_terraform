e8cfa448649b67d88d6dbd90966768f625d47948
Bart Wolinski <99290282+bwolinsk@users.noreply.github.com>
2023-04-05 22:19:41 -0400

    Update

--- tf/aws.tf
+++ tf/aws.tf
@@ -28,9 +28,42 @@ resource "aws_subnet" "tf_subnet" {
   }
 }
 
+resource "aws_security_group" "Terraform_Demo_SG" {
+  name        = var.name_tag
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0c9978668f8d55984"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.Terraform_Demo_SG.name]
   key_name      = "controller_bart"
   count         = "1"
   subnet_id     = aws_subnet.tf_subnet.id
