bf7d8128e00cd9c2eb7c984a915aefe39125eefc
Bart Wolinski <99290282+bwolinsk@users.noreply.github.com>
2023-10-10 23:23:39 -0400

    Update aws.tf

--- tf/aws.tf
+++ tf/aws.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "tf_subnet" {
 resource "aws_instance" "example" {
   ami = "ami-0c9978668f8d55984"
   instance_type = "t2.micro"
-  key_name      = "controller_bart"
+  key_name      = "insights_controller"
   count         = "1"
   subnet_id     = aws_subnet.tf_subnet.id
 }
